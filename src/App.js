2a5bd54c14fddad696965ae90a1fff94403c5a38
Clement Bourgoin <clement@actialuna.com>
2019-04-11 14:07:29 +0200

    Display browser and operating system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import UAParser from 'ua-parser-js';
+
 import './App.css';
+
+const parser = new UAParser(navigator.userAgent);
+const browser = parser.getBrowser();
+const os = parser.getOS();
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <p>
+          Your browser:
+          <br />
+          {browser.name} {browser.version}
+        </p>
+        <p>
+          Your operating system:
+          <br />
+          {os.name} {os.version}
+        </p>
+      </div>
+    );
   }
 }
 
